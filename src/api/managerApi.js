fe80b6f8daa8848213ce574aaa8945938909cc7a
이하름 <>
2024-02-18 17:37:59 +0900

    pull전커밋

--- src/api/managerApi.js
+++ src/api/managerApi.js
@@ -1,6 +1,6 @@
 import http from "./instance";
 
-// GET
+//GET_유저,사용자 리스트/상세리스트
 export const getUserList = () => http.get(`/api/master/user`);
 export const getMateList = () => http.get(`/api/master/mate`);
 export const getUserDetail = (userCid) =>
@@ -8,8 +8,25 @@ export const getUserDetail = (userCid) =>
 export const getMateDetail = (mateCid) =>
   http.get(`/api/master/mate/${mateCid}`);
 
-//POST
+//POST_mate 승인미승인
 export const approveMate = (mateCid) =>
   http.post(`/api/master/approve/${mateCid}`);
 export const unapproveMate = (mateCid) =>
   http.post(`/api/master/unapprove/${mateCid}`);
+
+//PUT_블랙리스트
+// export const userBlacklisted = (mateCid) =>
+//   http.put(`/api/master/user/${mateCid}`);
+
+// export const userBlacklisted = (userCid, isBlacklisted) =>
+//   http.put(`/api/master/user/${userCid}`, { blacklisted: isBlacklisted });
+export const userBlacklisted = (userCid, isBlacklisted) =>
+  http.put(`/api/master/user/${userCid}`, null, {
+    params: { blacklisted: isBlacklisted },
+  });
+
+// export const mateBlacklisted = (mateCid) =>
+//   http.put(`/api/master/mate/${mateCid}`);
+
+export const mateBlacklisted = (mateCid, isBlacklisted) =>
+  http.put(`/api/master/mate/${mateCid}`, { blacklisted: isBlacklisted });
