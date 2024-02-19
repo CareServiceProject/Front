695baaebe2bbc1fbf3727143c0f624bba80473f0
rose <113295030+ros-e2@users.noreply.github.com>
2024-02-19 12:02:27 +0900

    cancel

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -11,15 +11,23 @@ import {
   IonModal,
   IonTitle,
   IonToolbar,
-} from "@ionic/react";
-import { useEffect, useRef, useState } from "react";
-import DefaultAvatar from "../assets/default_avatar.jpg";
-import { Modal, Rate, Toast } from "antd-mobile";
-import { useNavigate } from "react-router-dom";
-import { userCancelService } from "../api/user";
-import { mateFinish, waitingDetail } from "../api/mateApi";
+} from '@ionic/react';
+import { useEffect, useRef, useState } from 'react';
+import DefaultAvatar from '../assets/default_avatar.jpg';
+import { Modal, Rate, Toast } from 'antd-mobile';
+import { useNavigate } from 'react-router-dom';
+import { userCancelService } from '../api/user';
+import { mateFinish, waitingDetail } from '../api/mateApi';
 
-const ServiceCard = ({ data, role, status, onAccept, onReload, onRate }) => {
+const ServiceCard = ({
+  data,
+  role,
+  status,
+  onAccept,
+  onReload,
+  onRate,
+  careCid,
+}) => {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
   const [rate, setRate] = useState(0);
@@ -27,22 +35,22 @@ const ServiceCard = ({ data, role, status, onAccept, onReload, onRate }) => {
 
   const Contens = ({ isDetail }) => {
     const label = isDetail
-      ? ["내용", "일정", "위치", "성별", "금액"]
-      : ["내용", "일정", "위치"];
+      ? ['내용', '일정', '위치', '성별', '금액']
+      : ['내용', '일정', '위치'];
 
     const contentShow = (item) => {
       switch (item) {
-        case "내용":
+        case '내용':
           return datas.content;
-        case "일정":
-          return role === "user"
+        case '일정':
+          return role === 'user'
             ? datas.date
-            : datas.date + "/" + datas.startTime;
-        case "위치":
-          return role === "user" ? datas.location : datas.arrivalLoc;
-        case "성별":
+            : datas.date + '/' + datas.startTime;
+        case '위치':
+          return role === 'user' ? datas.location : datas.arrivalLoc;
+        case '성별':
           return datas.gender;
-        case "금액":
+        case '금액':
           return datas.cost;
         default:
           return datas.content;
@@ -53,22 +61,34 @@ const ServiceCard = ({ data, role, status, onAccept, onReload, onRate }) => {
       const userCancel = () => {
         userCancelService(id).then((res) => {
           Toast.show({
-            content: "성공적으로 취소되었습니다.",
+            content: '성공적으로 취소되었습니다.',
           });
           setTimeout(() => {
             onReload();
           }, 1000);
         });
       };
-      const mateCancel = () => {};
+      const mateCancel = () => {
+        const mateCancel = () => {
+          if (typeof onCancelHelpRequest === 'function') {
+            onCancelHelpRequest(id).then((res) => {
+              console.log(res);
+              Toast.show({
+                content: '도움 취소되었습니다.',
+              });
+              onReload();
+            });
+          }
+        };
+      };
 
-      role === "user" ? userCancel() : mateCancel();
+      role === 'user' ? userCancel() : mateCancel();
     };
 
     const onFinish = (id) => {
       mateFinish(id).then((res) => {
         Toast.show({
-          content: "완료되었습니다.",
+          content: '완료되었습니다.',
         });
         setInterval(() => {
           onReload();
@@ -84,20 +104,20 @@ const ServiceCard = ({ data, role, status, onAccept, onReload, onRate }) => {
     return (
       <IonCard color="secondary">
         <IonCardHeader>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <IonAvatar
               className="ion-margin-end"
-              style={{ width: "70px", height: "70px" }}
+              style={{ width: '70px', height: '70px' }}
             >
               <img src={data.imageAddress || DefaultAvatar}></img>
             </IonAvatar>
-            {(status === "proceeding" ||
-              status === "completed" ||
-              status === "IN_PROGRESS" ||
-              status === "HELP_DONE") && (
+            {(status === 'proceeding' ||
+              status === 'completed' ||
+              status === 'IN_PROGRESS' ||
+              status === 'HELP_DONE') && (
               <IonButton
                 fill="clear"
-                onClick={() => navigate("/mate/chatting")}
+                onClick={() => navigate('/mate/chatting')}
               >
                 채팅
               </IonButton>
@@ -110,19 +130,19 @@ const ServiceCard = ({ data, role, status, onAccept, onReload, onRate }) => {
             return (
               <div
                 key={index}
-                style={{ display: "flex", alignItems: "center" }}
+                style={{ display: 'flex', alignItems: 'center' }}
                 className="ion-margin"
               >
                 <IonLabel>{item}</IonLabel>
 
                 <div
                   style={{
-                    display: "flex",
+                    display: 'flex',
                     flex: 1,
-                    borderRadius: "8px",
-                    backgroundColor: "white",
-                    padding: "5px",
-                    color: "black",
+                    borderRadius: '8px',
+                    backgroundColor: 'white',
+                    padding: '5px',
+                    color: 'black',
                   }}
                   className="ion-margin-start"
                 >
@@ -134,66 +154,72 @@ const ServiceCard = ({ data, role, status, onAccept, onReload, onRate }) => {
         </IonCardContent>
         <div
           style={{
-            display: "flex",
-            justifyContent: "flex-end",
-            flexWrap: "wrap",
+            display: 'flex',
+            justifyContent: 'flex-end',
+            flexWrap: 'wrap',
           }}
           className="ion-margin"
         >
-          {!isDetail && role === "mate" && (
+          {!isDetail && role === 'mate' && (
             <IonButton fill="clear" onClick={onDetailClick}>
               상세 내역
             </IonButton>
           )}
 
           {/* 유저 && 대기중 || 진행중 */}
-          {((role === "user" && status === "waiting") ||
-            status === "proceeding" ||
-            status === "IN_PROGRESS") && (
-            <IonButton fill="clear" onClick={() => oncancel(data.careCid)}>
+          {((role === 'user' && status === 'waiting') ||
+            status === 'proceeding' ||
+            status === 'IN_PROGRESS') && (
+            <IonButton
+              fill="clear"
+              onClick={() => {
+                oncancel(data.careCid);
+                console.log('도움 취소 버튼이 클릭되었습니다.');
+              }}
+            >
               취소
             </IonButton>
           )}
 
-          {role === "mate" && status === "IN_PROGRESS" && (
+          {role === 'mate' && status === 'IN_PROGRESS' && (
             <IonButton fill="clear" onClick={() => onFinish(data.careCid)}>
               완료
             </IonButton>
           )}
           {/* 메이트 && 완료 */}
-          {role === "mate" && status === "HELP_DONE" && (
+          {role === 'mate' && status === 'HELP_DONE' && (
             <>
               <IonButton
                 fill="clear"
                 onClick={() => {
                   Modal.show({
-                    header: "결제가 되었습니까?",
+                    header: '결제가 되었습니까?',
 
                     closeOnMaskClick: true,
                     closeOnAction: true,
                     actions: [
                       {
-                        key: "paymentDone",
-                        text: "결제완료",
+                        key: 'paymentDone',
+                        text: '결제완료',
                         primary: true,
                         onClick() {
                           //
                         },
                         style: {
-                          backgroundColor: "var(--ion-color-primary)",
-                          border: "none",
+                          backgroundColor: 'var(--ion-color-primary)',
+                          border: 'none',
                         },
                       },
                       {
-                        key: "paymentNo",
-                        text: "결제미완",
+                        key: 'paymentNo',
+                        text: '결제미완',
                         primary: true,
                         onClick() {
                           //
                         },
                         style: {
-                          backgroundColor: "var(--ion-color-danger)",
-                          border: "none",
+                          backgroundColor: 'var(--ion-color-danger)',
+                          border: 'none',
                         },
                       },
                     ],
@@ -206,7 +232,7 @@ const ServiceCard = ({ data, role, status, onAccept, onReload, onRate }) => {
           )}
 
           {/* 메이트 && 대기중 */}
-          {role === "mate" && status === "waiting" && (
+          {role === 'mate' && status === 'waiting' && (
             <IonButton
               fill="clear"
               onClick={() => {
@@ -218,15 +244,15 @@ const ServiceCard = ({ data, role, status, onAccept, onReload, onRate }) => {
           )}
 
           {/* 유저 && 완료 */}
-          {role === "user" && status === "completed" && (
+          {role === 'user' && status === 'completed' && (
             <IonButton
               fill="clear"
               onClick={() => {
                 Modal.show({
-                  title: "오늘 동행해드린 메이트는 어떠셨나요?",
-                  header: "평가해주세요!",
+                  title: '오늘 동행해드린 메이트는 어떠셨나요?',
+                  header: '평가해주세요!',
                   content: (
-                    <div style={{ display: "flex", justifyContent: "center" }}>
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
                       <Rate
                         allowHalf
                         onChange={(v) => {
@@ -240,15 +266,15 @@ const ServiceCard = ({ data, role, status, onAccept, onReload, onRate }) => {
                   closeOnAction: true,
                   actions: [
                     {
-                      key: "submit",
-                      text: "제출하기",
+                      key: 'submit',
+                      text: '제출하기',
                       primary: true,
                       onClick() {
                         onRate(data.careCid, rate);
                       },
                       style: {
-                        backgroundColor: "var(--ion-color-primary)",
-                        border: "none",
+                        backgroundColor: 'var(--ion-color-primary)',
+                        border: 'none',
                       },
                     },
                   ],
--- src/pages/mate/mateMy/index.jsx
+++ src/pages/mate/mateMy/index.jsx
@@ -10,16 +10,16 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-} from "@ionic/react";
-import React, { useEffect, useState } from "react";
-import { Rate } from "antd-mobile";
-import StatusCard from "../../../components/StatusCard";
-import DefaultAvatar from "../../../assets/default_avatar.jpg";
-import { mateCareHistory, mateInfo, mateMy } from "../../../api/mateApi";
+} from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import { Rate } from 'antd-mobile';
+import StatusCard from '../../../components/StatusCard';
+import DefaultAvatar from '../../../assets/default_avatar.jpg';
+import { mateCareHistory, mateMy } from '../../../api/mateApi';
+import { cancelHelpRequest } from '../../../api/mateApi';
 
 const MateMy = () => {
   const [data, setData] = useState({});
-  const [info, setInfo] = useState({});
 
   useEffect(() => {
     const requestInfo = () => {
@@ -27,15 +27,17 @@ const MateMy = () => {
         setData(res);
       });
     };
-
-    const myInfo = () => {
-      mateInfo().then((res) => {
-        setInfo(res);
-      });
-    };
     requestInfo();
-    myInfo();
   }, []);
+  const handleMateCancel = (careCid) => {
+    cancelHelpRequest(careCid)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <IonPage id="main-menu">
       <IonHeader>
@@ -46,32 +48,24 @@ const MateMy = () => {
           <IonTitle>Page Title</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding ">
-        <div
-          style={{ display: "flex", alignItems: "center" }}
-          className="ion-margin"
-        >
-          <IonAvatar
-            className="ion-margin-end"
-            style={{ width: "80px", height: "80px" }}
-          >
-            <img src={data.imageAddress || DefaultAvatar}></img>
+      <IonContent className="ion-padding">
+        <div style={{ display: 'flex' }} className="ion-margin">
+          <IonAvatar className="ion-margin-end">
+            <img src={DefaultAvatar}></img>
           </IonAvatar>
-          <h2>{info.name}님</h2>
+          <h2>000님</h2>
         </div>
         <IonCard>
           <IonCardHeader>
             <h6>내 별점</h6>
           </IonCardHeader>
           <IonCardContent>
-            <Rate readOnly value={data.mateRating} allowHalf />{" "}
+            <Rate readOnly value={data.mateRating} allowHalf />{' '}
             {data.mateRating}
-            {"점"}
+            {'점'}
           </IonCardContent>
         </IonCard>
-
-        <StatusCard data={data}></StatusCard>
-        <div style={{ height: "100px" }}></div>
+        <StatusCard data={data} onCancelHelpRequest={handleMateCancel} />
       </IonContent>
     </IonPage>
   );
